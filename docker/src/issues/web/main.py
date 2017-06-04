ddafde45be00d1510ef5aaea2451b06696aeec5b
Pablo Daniel Rey <pablodanielrey@gmail.com>
2017-06-04 05:00:33 -0300

    agregue un ejemplo del flujo de redirects para que funcione el login

--- docker/src/issues/web/main.py
+++ docker/src/issues/web/main.py
@@ -1,5 +1,5 @@
 from functools import wraps
-from flask import Flask, request, send_from_directory, g, redirect
+from flask import Flask, request, send_from_directory, g, redirect, make_response
 from flask_httpauth import HTTPBasicAuth
 
 # set the project root directory as the static folder, you can set others.
@@ -9,20 +9,49 @@ auth = HTTPBasicAuth()
 def redirect_not_authenticated(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-        app.logger.debug('redirect_not_authenticated')
+        app.logger.debug('redirect_not_authenticated {}'.format(g.authenticated))
         if not g.authenticated:
             app.logger.debug('no')
-            return redirect("http://login.econo.unlp.edu.ar", code=302)
+            """
+                1 - generar un hash temporal
+
+                hash = generate_hash()
+
+                2 - enviar al login ese hash + referer (request.url) para ser procesado
+
+                login.api.wamp.internal_hash(hash, referer, timeout)
+
+                3 - retornar al cliente un redirect al login + parametro = hash
+
+                return redirect("http://login.econo.unlp.edu.ar?t={}".format(hash), code=302)
+            """
+            token = 'dfsdfd'
+            return redirect("http://login.econo.unlp.edu.ar?t={}".format(token), code=302)
         else:
-            app.logger.debug('yes')
-            return f(*args, **kwargs)
+            if g.authenticated == 'algo':
+                return make_response(request.url, 200)
+            else:
+                app.logger.debug('yes')
+                resp = make_response(redirect("/index"))
+                resp.set_cookie('token', value='algo')
+                return resp
+                """ return f(*args, **kwargs) """
     return decorated
 
 @auth.verify_password
 def verify_password(username, password):
     app.logger.debug('verify_password {}'.format(username))
+
+    """ chequeo si existe la cookie token """
+    t = request.cookies.get('token')
+    if t:
+        g.authenticated = t
+        return True
+
+    """ chequeo info de usuario """
     if username == '':
         g.authenticated = False
+        return False
     else:
         g.authenticated = username
     return True
