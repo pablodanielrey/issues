5d97178638e0f1f12ad8b9ed95f0ac1bde51cbc1
Emanuel <emanuel.pais@econo.unlp.edu.ar>
2017-11-10 13:48:36 -0300

    Modifique el registrar.py porque tenia las referencias a users

--- docker/src/issues/registrar.py
+++ docker/src/issues/registrar.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     import os
     import sys
 
-    dominio = os.environ['USERS_URL'].replace('http://','').replace('https://','')
+    dominio = os.environ['ISSUES_URL'].replace('http://','').replace('https://','')
     nombre = sys.argv[1]
     path = sys.argv[2]
     server = sys.argv[3]
